40795decff7844a9404a5eb463ade71cb882f4d3
svgaming234 <sven.kovac1@outlook.com>
2026-01-23 21:27:16 +0100

    fix auctionreset not working from console, add error for bid from console, log auction messages to console

--- src/main/java/org/garsooon/Commands/BidCommand.java
+++ src/main/java/org/garsooon/Commands/BidCommand.java
@@ -15,7 +15,10 @@ public class BidCommand implements CommandExecutor {
     }
 
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (!(sender instanceof Player)) return true;
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(ChatColor.RED + "Only players can use this command.");
+            return true;
+        }
 
         Player player = (Player) sender;
 
